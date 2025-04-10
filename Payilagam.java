d6fc8f3ad9edda7acd76cf8c06c19e8e18a33ad0
sudhakarrockzz564 <sudhakarrockzz564@gmail.com>
2025-04-10 12:20:22 +0530

    test

---
+++ Payilagam.java
@@ -0,0 +1,31 @@
+public class Payilagam {  
+    String course;  
+    int age;  
+    String address;  
+
+    public static void main(String[] args) {  
+        Payilagam ram = new Payilagam("Java", 25, "Chennai");  
+        Payilagam sam = new Payilagam();  
+        
+
+        sam.displayInfo();  
+    }  
+
+    public Payilagam(String course, int age, String address) {  
+        course = course;  
+        age = age;  
+        address = address;  
+    }  
+
+    public Payilagam() {  
+        course = "Unknown";  
+        age = 0;  
+        address = "Not Specified";  
+    }  
+
+    public void displayInfo() {  
+        System.out.println("Course: " + course);  
+        System.out.println("Age: " + age);  
+        System.out.println("Address: " + address);  
+    }  
+}  
