8b470a1279dca397d57e3c350fde682750df3d27
sudhakarrockzz564 <sudhakarrockzz564@gmail.com>
2025-04-10 21:37:44 +0530

    practice

---
+++ JAVA/Dosa.java
@@ -0,0 +1,13 @@
+public class Dosa {
+public static void main(String[]args) {
+int balance = 8;
+int count = 0;
+int eaten;	
+while(count<=3) {
+count = balance/2;
+eaten= balance+count;
+count++;
+System.out.println(balance);
+}
+}
+}
