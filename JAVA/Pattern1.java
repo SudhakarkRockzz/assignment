71eded639227b77c8c59fbb5698b36c245fce7da
sudhakarrockzz564 <sudhakarrockzz564@hmail.com>
2025-05-01 17:46:50 +0530

    d

--- JAVA/Pattern1.java
+++ JAVA/Pattern1.java
@@ -1,16 +1,14 @@
+package com.Example;
+
 public class Pattern1 {
-
-public static void main(String[]args) {
- Pattern s = new Pattern();
-int row = 1;
-while(row<=4){
-for(int col=1;col<=5;col++) {
-System.out.println(col+"");
-
-row++;
-System.out.println(col+"");
+	public static void main(String[]args) {
+		
+		for(int row= 1;row<=5;row++) {
+			for(int col= 1 ;col<=6-row;col++) {
+			System.out.print(col+row-1 +"  ");
+		}
+			System.out.println();
+	}
+		
 }
-}
-}
-}
-
+} 
